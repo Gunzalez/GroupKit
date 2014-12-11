99ed4bd7e716d9d555c03007fe19ae1621c97c52
Segun Konibire <segun.konibire@springstudios.com>
2014-12-11 22:28:41 +0000

    Initial files and project set-up

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>Group Kit</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/groupkit.css">
+</head>
+<body>
+<div class="page">
+
+
+</div>
+
+    <script src="js/vendors/jquery-1.9.1.min.js"></script>
+    <script src="js/groupkit.js"></script>
+</body>
+</html>
