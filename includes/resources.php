67df9c9545581fd0facce883586804ec16a3219b
Segun Konibire <segun.konibire@springstudios.com>
2014-12-18 02:20:38 +0000

    Choose Garment, responsive

--- includes/resources.php
+++ includes/resources.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gunzalez
- * Date: 16/12/2014
- * Time: 23:56
- */ 
+<meta charset="utf-8">
+<meta name="keywords" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/groupkit.css">
--- members-sign-in.php
+++ members-sign-in.php
@@ -2,28 +2,22 @@
 <html>
 <head>
     <title>Group Kit- Members sign in</title>
-    <meta charset="utf-8">
-    <meta name="keywords" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/groupkit.css">
+    <?php include_once('includes/resources.php'); ?>
 </head>
-<body class="members-sign-in">
+<body class="members-sign-in page">
 
     <?php include_once('includes/header.php'); ?>
 
     <div class="main">
         <div class="content">
 
-            <div class="title">
+            <div class="title form-heading">
                 <h2>Members sign in</h2>
-                <a href="#" class="button back-button">Back</a>
+                <a href="#" class="button back-button white-button">Back</a>
                 <div class="cboth"></div>
             </div>
 
-            <form class="form" id="member-sign-in-form" action="" method="post" name="members-sign-in-form" novalidate="novalidate">
+            <form class="form" id="member-sign-in-form" action="members-choose-garment.php" method="post" name="members-sign-in-form" novalidate="novalidate">
                 <div class="row">
                     <label for="email">Email address</label>
                     <input type="email" name="email" id="email" value="" class="input" />
@@ -36,10 +30,11 @@
                     <input type="submit" value="Sign in" class="button" />
                 </div>
                 <div class="cleft"></div>
+                <div class="divider"></div>
             </form>
 
-            <h3>Forgotten your password?</h3>
-            <form class="form" id="forgot-password" action="" method="post" name="forgot-password" novalidate="novalidate">
+            <h3 class="form-subheading">Forgotten your password?</h3>
+            <form class="form last-element" id="forgot-password" action="" method="post" name="forgot-password" novalidate="novalidate">
                 <div class="row">
                     <label for="email">Email address</label>
                     <input type="email" name="email" id="email" value="" class="input" />
