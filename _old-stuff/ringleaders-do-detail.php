1c8cd4ae746e6bc27e7bcfe68c16d77da1c91afe
Segun Konibire <segun.konibire@springstudios.com>
2015-02-07 13:48:39 +0000

    Folder tidy

--- _old-stuff/ringleaders-do-detail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$isNew = $_POST['teamId'];
-if($isNew == 'new'){
-    header( 'Location: ringleaders-edit-order.php?id=111' );
-} else {
-    header( 'Location: ringleaders-edit-order.php?id='.$isNew );
-}
-?>
